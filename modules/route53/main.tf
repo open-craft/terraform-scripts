8606d309873cd5ccf57887db386c0d184d962171
Raul Gallegos <raul@opencraft.com>
2020-12-29 16:11:16 -0500

    SE-3766: updates how to properly create a Certificate

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -6,15 +6,19 @@ resource aws_route53_zone "primary" {
   }
 }
 
-# this was imported
 resource aws_acm_certificate "all_subdomains_certificate" {
+  domain_name = var.customer_domain
+  validation_method = "DNS"
+
   lifecycle {
     prevent_destroy = true
   }
 }
 
-# this was imported
 resource aws_acm_certificate "main_domain_certificate" {
+  domain_name = "*.${var.customer_domain}"
+  validation_method = "DNS"
+
   lifecycle {
     prevent_destroy = true
   }
