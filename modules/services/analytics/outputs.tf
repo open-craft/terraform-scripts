5a575f47cf42c649b9f92674f21003095033aeff
Raul Gallegos <raul@opencraft.com>
2021-04-06 18:07:37 -0500

    FAL-1775: adds instance information output

--- modules/services/analytics/outputs.tf
+++ modules/services/analytics/outputs.tf
@@ -9,3 +9,11 @@ output "emr_rds_security_group_id" {
 output "elasticsearch_host" {
   value = module.elasticsearch.elasticsearch
 }
+
+output "analytics_instance_private_ips" {
+  value = aws_instance.analytics.*.private_ip
+}
+
+output "analytics_instance_public_ips" {
+  value = aws_instance.analytics.*.public_ip
+}
