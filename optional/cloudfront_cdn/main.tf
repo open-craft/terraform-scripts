bb414e02a31b834657550e6331366cc9069a7062
Agrendalath <piotr@surowiec.it>
2022-03-20 17:34:06 +0100

    feat: allow using custom CDN domains

--- optional/cloudfront_cdn/main.tf
+++ optional/cloudfront_cdn/main.tf
@@ -1,7 +1,15 @@
+# Custom aliases require setting a custom ACM certificate, which needs to be in the `us-east-1` region.
+provider "aws" {
+  alias   = "acm"
+  region  = "us-east-1"
+  profile = var.aws_provider_profile
+}
+
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   enabled             = true
   is_ipv6_enabled     = true
   comment             = lower(join("-", [var.client_shortname, var.environment, var.service_name]))
+  aliases             = var.aliases
 
   origin {
     domain_name         = var.origin_domain
@@ -55,6 +63,83 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    # This part is very ugly because there are 3 typical use cases:
+    #   1. Using the default certificate domain.
+    #      In this case, no additional resources are needed because the CloudFront default certificate is sufficient.
+    #   2. Use the existing ACM certificate from the `us-east-1` region.
+    #      In this case, only the domain record is created.
+    #   3. Generate a new ACM certificate in the `us-east-1` region.
+    #      Applicable when the main certificate already exists in another region.
+    cloudfront_default_certificate = var.aliases == null ? true : false
+
+    acm_certificate_arn = (
+                            var.aliases == null
+                              ? null
+                              : var.alias_certificate_arn == null
+                                ? aws_acm_certificate.alias_certificate[0].arn
+                                  : var.alias_certificate_arn
+                          )
+    # Needs to be set along with the non-default certificate.
+    ssl_support_method = var.aliases == null ? null : "sni-only"
   }
+}
+
+
+# Custom CDN subdomain.
+resource "aws_route53_record" alias {
+  count = var.alias_zone_id == null ? 0 : 1
+
+  zone_id = var.alias_zone_id
+  name    = var.alias_name
+  type    = "A"
+
+  alias {
+    name    = aws_cloudfront_distribution.cloudfront_distribution.domain_name
+    zone_id = aws_cloudfront_distribution.cloudfront_distribution.hosted_zone_id
+
+    evaluate_target_health = false
+  }
+}
+
+# Custom ACM certificate.
+resource "aws_acm_certificate" alias_certificate {
+  count = var.alias_zone_id != null && var.alias_certificate_arn == null ? 1 : 0
+  provider = aws.acm
+
+  domain_name       = "${var.alias_name}.${var.origin_domain}"
+  validation_method = "DNS"
+}
+
+locals {
+  domain_validation_options = (length(aws_acm_certificate.alias_certificate) > 0
+                                 ? aws_acm_certificate.alias_certificate[0].domain_validation_options
+                                 : []
+                              )
+}
+
+resource aws_route53_record alias_validation_records {
+  for_each = {
+    for dvo in local.domain_validation_options : dvo.domain_name => {
+      name = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  }
+  }
+
+  allow_overwrite = true
+  zone_id = var.alias_zone_id
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [
+    each.value.record
+  ]
+}
+
+resource "aws_acm_certificate_validation" alias_domain_validation {
+  provider = aws.acm
+  count    = length(aws_acm_certificate.alias_certificate)
+
+  certificate_arn         = aws_acm_certificate.alias_certificate[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.alias_validation_records : record.fqdn]
 }
